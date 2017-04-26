2cbb86ec43a70a9e7a6f0a29e62519d75d4e2c82
Qwerty Qwerty <Qwerty Qwerty>
2017-04-26 22:32:01 +0300

    added window

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
-
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        JFrame frame = new JFrame ("MyShapeViewer");
+        frame.setSize(frame.getContentPane().getPreferredSize());
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE );
+        frame.setSize(640, 480);
+        frame.setVisible(true);
     }
 
 }
